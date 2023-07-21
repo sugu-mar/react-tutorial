09fcae29ada57ec69e90805a375c4da0c9c8331e
sugu-mar <sugumargpt@gmail.com>
2023-07-21 16:52:10 +0530

    Toggle summary button added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css'
-import { React, useEffect } from 'react'
+import { React, useEffect, useState } from 'react'
 import { Msg } from './components/Msg'
 import { ColorGame } from './components/ColorGame'
+import { Counter } from './components/Counter'
 
 const initialMovieList = [
   {
@@ -12,6 +13,31 @@ const initialMovieList = [
     summary:
       'RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.',
     trailer: 'https://www.youtube.com/embed/f_vbAtFSEc0',
+  },
+  {
+    name: 'Interstellar',
+    poster: 'https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg',
+    rating: 8.6,
+    summary:
+      'When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.',
+    trailer: 'https://www.youtube.com/embed/zSWdZVtXT7E',
+  },
+  {
+    name: 'Baahubali',
+    poster: 'https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg',
+    rating: 8,
+    summary:
+      'In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.',
+    trailer: 'https://www.youtube.com/embed/sOEg_YZQsTI',
+  },
+  {
+    name: 'The Avengers',
+    rating: 8,
+    summary:
+      "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
+    poster:
+      'https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg',
+    trailer: 'https://www.youtube.com/embed/eOrNdBpGMv8',
   },
   {
     name: 'Iron man 2',
@@ -40,31 +66,7 @@ const initialMovieList = [
     rating: 8.8,
     trailer: 'https://www.youtube.com/embed/nnXpbTFrqXA',
   },
-  {
-    name: 'The Avengers',
-    rating: 8,
-    summary:
-      "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-    poster:
-      'https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg',
-    trailer: 'https://www.youtube.com/embed/eOrNdBpGMv8',
-  },
-  {
-    name: 'Interstellar',
-    poster: 'https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg',
-    rating: 8.6,
-    summary:
-      'When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.',
-    trailer: 'https://www.youtube.com/embed/zSWdZVtXT7E',
-  },
-  {
-    name: 'Baahubali',
-    poster: 'https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg',
-    rating: 8,
-    summary:
-      'In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.',
-    trailer: 'https://www.youtube.com/embed/sOEg_YZQsTI',
-  },
+
   {
     name: 'Ratatouille',
     poster:
@@ -79,31 +81,29 @@ const initialMovieList = [
 function App() {
   const movieList = initialMovieList
 
-  const user = [
-    {
-      name: 'sachin',
-      pic:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Sachin-Tendulkar_%28cropped%29.jpg/330px-Sachin-Tendulkar_%28cropped%29.jpg',
-    },
-    {
-      name: 'dhoni',
-      pic:
-        'https://upload.wikimedia.org/wikipedia/commons/7/70/Mahendra_Singh_Dhoni_January_2016_%28cropped%29.jpg',
-    },
-    {
-      name: 'vk',
-      pic:
-        'https://upload.wikimedia.org/wikipedia/commons/e/ef/Virat_Kohli_during_the_India_vs_Aus_4th_Test_match_at_Narendra_Modi_Stadium_on_09_March_2023.jpg',
-    },
-    {
-      name: 'rohit',
-      pic:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Rohit_Sharma_during_the_India_vs_Australia_4th_Test_match_at_Narendra_Modi_Stadium.jpg/330px-Rohit_Sharma_during_the_India_vs_Australia_4th_Test_match_at_Narendra_Modi_Stadium.jpg',
-    },
-  ]
+  // const user = [
+  //   {
+  //     name: 'sachin',
+  //     pic:
+  //       'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Sachin-Tendulkar_%28cropped%29.jpg/330px-Sachin-Tendulkar_%28cropped%29.jpg',
+  //   },
+  //   {
+  //     name: 'dhoni',
+  //     pic:
+  //       'https://upload.wikimedia.org/wikipedia/commons/7/70/Mahendra_Singh_Dhoni_January_2016_%28cropped%29.jpg',
+  //   },
+  //   {
+  //     name: 'vk',
+  //     pic:
+  //       'https://upload.wikimedia.org/wikipedia/commons/e/ef/Virat_Kohli_during_the_India_vs_Aus_4th_Test_match_at_Narendra_Modi_Stadium_on_09_March_2023.jpg',
+  //   },
+  //   {
+  //     name: 'rohit',
+  //     pic:
+  //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Rohit_Sharma_during_the_India_vs_Australia_4th_Test_match_at_Narendra_Modi_Stadium.jpg/330px-Rohit_Sharma_during_the_India_vs_Australia_4th_Test_match_at_Narendra_Modi_Stadium.jpg',
+  //   },
+  // ]
 
-  // const pic =
-  // const name = "sagu"
   return (
     <div className="App">
       {/* <Welcome name={name} /> */}
@@ -131,8 +131,8 @@ function App() {
       ))}
       <ColorGame /> */}
       <div className="movie-list">
-        {movieList.map((mov) => (
-          <Movie movie={mov} />
+        {movieList.map((mov, index) => (
+          <Movie movie={mov} key={index} />
         ))}
       </div>
     </div>
@@ -140,6 +140,10 @@ function App() {
 }
 
 function Movie({ movie }) {
+  const [show, setShow] = useState(true)
+  // const togglebtnstyle = {
+  //   display: show ? 'block' : 'none',
+  // }
   return (
     <div className="movie-container ">
       <img src={movie.poster} className="movie-poster" />
@@ -147,7 +151,16 @@ function Movie({ movie }) {
         <h2 className="movie-title">{movie.name}</h2>
         <p className="movie-rating">{movie.rating}</p>
       </div>
-      <p className="movie-summary">{movie.summary}</p>
+      <button onClick={() => setShow(!show)}>
+        {show ? 'hide description' : 'show description'}
+      </button>
+      {/* <p className="movie-summary" style={togglebtnstyle}>
+        {movie.summary}
+      </p> */}
+
+      {show ? <p className="movie-summary">{movie.summary}</p> : ''}
+
+      <Counter />
     </div>
   )
 }
